6c168e6765ef4e2de22253b584c85ef9a12f9f06
Yui <yuiko.k.0116@gmail.com>
2024-08-24 16:18:52 +0200

    added day3-1

---
+++ day3/day3-1.py
@@ -0,0 +1,38 @@
+import string
+
+lowercase = list(string.ascii_lowercase)
+uppercase = list(string.ascii_uppercase)
+
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
+
+K = 2
+score = 0
+
+for line in lines:
+    for cha in line.splitlines():
+        line_len = len(cha)
+        chunk_len = line_len // K
+        res = []
+        for c in range(0, line_len, chunk_len):
+            res.append(line[c: c + chunk_len])
+        pairs = []
+        for item in res:
+            set_item = list(set(item))
+            pairs += set_item
+        dup = []
+        for l in pairs:
+            if pairs.count(l) == 2 and l not in dup:
+                dup.append(l)
+            elif pairs.count(l) == 2 and l in dup:
+                continue
+        dup = ' '.join(dup)
+        if dup in lowercase:
+            score += lowercase.index(dup) + 1
+        elif dup in uppercase:
+            score += uppercase.index(dup) + 27
+
+print(score)
+
+
+
